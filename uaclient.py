f83a4f8b2faa1252abacbe83db9e88774052ae8e
Alba <alba@ubuntu.(none)>
2014-12-23 18:02:35 +0100

    Ahora importamos XMLHandler del uaserver

--- uaclient.py
+++ uaclient.py
@@ -3,41 +3,9 @@
 
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
+from uaserver import XMLHandler
 import sys
 
-
-class XMLHandler(ContentHandler):
-
-    def __init__(self):
-        """
-        Constructor. Inicializamos las variables
-        """
-        self.XML = {
-            'account': ['username', 'passwd'],
-            'uaserver': ['ip', 'puerto'],
-            'rtpaudio': ['puerto'],
-            'regproxy': ['ip', 'puerto'],
-            'log': ['path'],
-            'audio': ['path']
-        }
-
-        self.config = {}
-
-    def startElement(self, name, attrs):
-        """
-        Método que se llama cuando se abre una etiqueta
-
-        """
-        if name in self.XML:
-            for attr in self.XML[name]:
-                self.config[name + "_" + attr] = attrs.get(attr, "")
-                if name + "_" + attr == 'uaserver_ip':
-                    if self.config['uaserver_ip'] == "":
-                        self.config['uaserver_ip'] = '127.0.0.1'
-
-    def get_tags(self):
-        return self.config
-    
 try:
     CONFIG = sys.argv[1]
     METHOD = sys.argv[2].upper()
@@ -52,5 +20,6 @@ if __name__ == "__main__":
     parser = make_parser()
     cHandler = XMLHandler()
     parser.setContentHandler(cHandler)
-    parser.parse(open('ua1.xml'))
-    print cHandler.get_tags()
+    parser.parse(open(CONFIG))
+    UA = cHandler.get_tags()
+    print UA['account_username']
