9f177d5feb842d6e431988584ffc4af20f60f80a
Alba <alba@ubuntu.(none)>
2014-12-25 20:37:40 +0100

    uaclient.py casi acabado

--- uaclient.py
+++ uaclient.py
@@ -29,21 +29,41 @@ if __name__ == "__main__":
     # Enviamos diferentes cosas según el método
     if METHOD == "REGISTER":
         LINE = UA['account_username'] + ":" + UA['uaserver_puerto']
-        print "Enviando: " + METHOD + " sip:" + LINE + " SIP/2.0"
+        print "Enviando:\r\n" + METHOD + " sip:" + LINE + " SIP/2.0"
         print "Enviando: " + "EXPIRES:" + str(OPTION) + '\r\n\r\n'
         LINE_EXPIRES = "Expires: " + str(OPTION) + '\r\n\r\n'
         my_socket.send(METHOD + " sip:" + LINE + " SIP/2.0\r\n" + LINE_EXPIRES)
+        
     elif METHOD == "INVITE":
-        print "Enviando: " + METHOD + " sip:" + OPTION + " SIP/2.0"
-        print "Enviando: Content type: application/sdp" 
+        print "Enviando:\r\n" + METHOD + " sip:" + OPTION + " SIP/2.0"
+        print "Content type: application/sdp\r\n\r\n" 
         APPLICATION = "Content type:application/sdp" + "\r\n\r\n"
-        my_socket.send(METHOD + " sip:" + OPTION + " SIP/2.0\r\n" + APPLICATION)
+        CABECERAS = METHOD + " sip:" + OPTION + " SIP/2.0\r\n" + APPLICATION
+        O = "o=" + UA['account_username'] + " " + UA['uaserver_ip'] + " \r\n"
+        M = "m=audio " + UA['rtpaudio_puerto'] + " RTP\r\n"
+        CUERPO = "v=0\r\n" + O + "s=mysession\r\n" + "t=0\r\n" + M
+        print CUERPO
+        my_socket.send(CABECERAS + CUERPO)
+        
     elif METHOD == "BYE":
-        print "Enviando: " + METHOD + " sip:" + OPTION + " SIP/2.0"
+        print "Enviando:\r\n" + METHOD + " sip:" + OPTION + " SIP/2.0"
         my_socket.send(METHOD + " sip:" + OPTION + " SIP/2.0\r\n\r\n")
+   
     try:
         data = my_socket.recv(1024)
     except socket.error:
         SOCKET_ERROR = UA['regproxy_ip'] + " PORT:" + UA['regproxy_puerto']
         sys.exit("No server listening at " + SOCKET_ERROR)
-    print 'Recibido -- ', data
+    print 'Recibido\r\n', data
+    
+    if METHOD == "INVITE":
+        #He recibido los tres mensajes a la vez, en una misma línea
+        METHOD = 'ACK'
+        print "Enviando: " + METHOD + " sip:" + OPTION + " SIP/2.0\r\n\r\n"
+        my_socket.send(METHOD + " sip:" + OPTION + " SIP/2.0\r\n\r\n")
+
+print "Terminando socket..."
+# Cerramos todo
+my_socket.close()
+print "Fin."        
+
