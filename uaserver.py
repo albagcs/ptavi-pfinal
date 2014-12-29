ef5b1417d26ff7dd9485dc4d46c1c070fce83774
Alba <alba@ubuntu.(none)>
2014-12-29 18:05:31 +0100

    uaserver.py con envío RTP

--- uaserver.py
+++ uaserver.py
@@ -56,6 +56,12 @@ class ServerHandler(SocketServer.DatagramRequestHandler):
             if not Method in METHODS:
                 self.wfile.write("SIP/2.0 405 Method Not Allowed\r\n\r\n")
             if Method == 'INVITE':
+                # Las utilizaremos para el envío de rtp
+                receptor_Ip = line.split("o=")[1].split(" ")[1].split("s")[0]
+                receptor_Puerto = line.split("m=")[1].split(" ")[1]
+                Rtp['receptor_Ip'] = receptor_Ip
+                Rtp['receptor_Puerto'] = receptor_Puerto
+                #Respondemos al invite
                 respuesta = "SIP/2.0 100 Trying\r\n\r\n"
                 respuesta += "SIP/2.0 180 Ringing\r\n\r\n"
                 respuesta += "SIP/2.0 200 OK\r\n"
@@ -66,11 +72,15 @@ class ServerHandler(SocketServer.DatagramRequestHandler):
                 CUERPO = "v=0\r\n" + O + " \r\ns=mysession\r\n" + "t=0\r\n" + M
                 respuesta += CUERPO
                 self.wfile.write(respuesta)
+               
             elif Method == 'ACK':
-                aEjecutar = './mp32rtp -i 127.0.0.1 -p 23032 < ' + sys.argv[3]
+                os.system("chmod 777 mp32rtp")
+                aEjecutar = './mp32rtp -i ' + Rtp['receptor_Ip'] + ' -p '
+                aEjecutar += Rtp['receptor_Puerto'] + " < " + UA['audio_path']
                 print "Vamos a ejecutar", aEjecutar
                 os.system(aEjecutar)
-                print(" Ha terminado la ejecución de fich de audio")
+                print("Ha terminado la ejecución de fich de audio")
+                
             elif Method == 'BYE':
                 respuesta = "SIP/2.0 200 OK\r\n\r\n"
                 self.wfile.write(respuesta)
@@ -82,6 +92,7 @@ if __name__ == "__main__":
         CONFIG = sys.argv[1]
     except IndexError:
         sys.exit("Usage: python server.py config")
+    Rtp = {}
     parser = make_parser()
     cHandler = XMLHandler()
     parser.setContentHandler(cHandler)
