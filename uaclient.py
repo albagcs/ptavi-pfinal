81a79008c58887548405e1b609426c75bd66f5f3
Alba <alba@ubuntu.(none)>
2015-01-06 16:25:18 +0100

    class Log, para el log

--- uaclient.py
+++ uaclient.py
@@ -9,6 +9,20 @@ import socket
 import os
 import time
 
+
+class Log(ContentHandler):
+    def Log(self, tipo, to, message):
+        fich = open(UA['log_path'], 'a')
+        Time = time.strftime('%Y%m%d%H%M%S', time.gmtime())
+        message = message.replace('\r\n', ' ') + '\n'
+        if tipo == 'send':
+            fich.write(Time + ' Sent to ' + to + message)
+        elif tipo == 'receive':
+            fich.write(Time + ' Received from ' + to + message)
+        elif tipo == 'error':
+            fich.write(Time + "Error: No server listening at " + message)
+        fich.close()
+
 if __name__ == "__main__":
     """
     Programa principal
@@ -31,16 +45,16 @@ if __name__ == "__main__":
     TO = UA['regproxy_ip'] + ':' + UA['regproxy_puerto'] + ': '
     # Enviamos diferentes cosas según el método
     if METHOD == "REGISTER":
+        try:
+            OPTION = int(OPTION)
+        except ValueError:
+            sys.exit("Usage: int OPTION Required")
         LINE = UA['account_username'] + ":" + UA['uaserver_puerto']
         EXPIRES = "Expires: " + str(OPTION) + '\r\n\r\n'
         SEND = METHOD + " sip:" + LINE + " SIP/2.0\r\n" + EXPIRES
         print "Enviado:\r\n" + METHOD + " sip:" + LINE + " SIP/2.0\n" + EXPIRES
         my_socket.send(SEND)
-        fich = open(UA['log_path'], 'a')
-        Time = time.strftime('%Y%m%d%H%M%S', time.gmtime())
-        START = 'Starting...\n'
-        fich.write(START + Time + ' Sent to ' + TO + SEND.replace('\r\n', ' '))
-        fich.close()
+        Log().Log('send', TO, SEND)
 
     elif METHOD == "INVITE":
         print "Enviando:\r\n" + METHOD + " sip:" + OPTION + " SIP/2.0"
@@ -51,37 +65,25 @@ if __name__ == "__main__":
         M = "m=audio " + UA['rtpaudio_puerto'] + " RTP\r\n"
         BODY = "v=0\r\n" + O + "s=mysession\r\n" + "t=0\r\n" + M
         print BODY
-        my_socket.send(HEAD + BODY)
-        BODY = BODY.replace('\r\n', ' ') + '\n'
-        fich = open(UA['log_path'], 'a')
-        Time = time.strftime('%Y%m%d%H%M%S', time.gmtime())
-        fich.write(Time + ' Sent to ' + TO + HEAD.replace("\r\n", " ") + BODY)
-        fich.close()
+        SEND = HEAD + BODY
+        my_socket.send(SEND)
+        Log().Log('send', TO, SEND)
 
     elif METHOD == "BYE":
-        LINE = METHOD + " sip:" + OPTION + " SIP/2.0\r\n\r\n"
+        SEND = METHOD + " sip:" + OPTION + " SIP/2.0\r\n\r\n"
         print "Enviando:\r\n" + METHOD + " sip:" + OPTION + " SIP/2.0"
-        my_socket.send(LINE)
-        fich = open(UA['log_path'], 'a')
-        Time = time.strftime('%Y%m%d%H%M%S', time.gmtime())
-        fich.write(Time + ' Sent to ' + TO + LINE.replace('\r\n', ' ') + '\n')
-        fich.close()
+        my_socket.send(SEND)
+        Log().Log('send', TO, SEND)
 
     try:
         data = my_socket.recv(1024)
     except socket.error:
         SOCKET_ERROR = UA['regproxy_ip'] + " PORT:" + UA['regproxy_puerto']
         sys.exit("Error: No server listening at " + SOCKET_ERROR)
-        fich = open(UA['log_path'], 'a')
-        Time = time.strftime('%Y%m%d%H%M%S', time.gmtime())
-        fich.write(Time + "Error: No server listening at " + SOCKET_ERROR)
+        Log().Log('error', TO, SOCKET_ERROR)
 
     print 'Recibido\r\n', data
-    DATA = data.replace("\r\n", " ") + '\n'
-    fich = open(UA['log_path'], 'a')
-    Time = time.strftime('%Y%m%d%H%M%S', time.gmtime())
-    fich.write(Time + ' Received from ' + TO + DATA)
-    fich.close()
+    Log().Log('receive', TO, data)
 
     if data == "SIP/2.0 404 User Not Found\r\n\r\n":
         sys.exit(" ")
@@ -89,8 +91,9 @@ if __name__ == "__main__":
         if METHOD == "INVITE":
             #He recibido los tres mensajes a la vez, en una misma línea
             METHOD = 'ACK'
+            SEND = METHOD + " sip:" + OPTION + " SIP/2.0\r\n\r\n"
             print "Enviando: " + METHOD + " sip:" + OPTION + " SIP/2.0\r\n\r\n"
-            my_socket.send(METHOD + " sip:" + OPTION + " SIP/2.0\r\n\r\n")
+            my_socket.send(SEND)
             receptor_Ip = data.split("o=")[1].split(" ")[1].split("s")[0]
             receptor_Puerto = data.split("m=")[1].split(" ")[1]
             os.system("chmod 777 mp32rtp")
@@ -99,15 +102,7 @@ if __name__ == "__main__":
             print "Vamos a ejecutar", aEjecutar
             os.system(aEjecutar)
             print("Ha terminado la ejecución de fich de audio")
-            fich = open(UA['log_path'], 'a')
-            Time = time.strftime('%Y%m%d%H%M%S', time.gmtime())
-            fich.write(Time + METHOD + " sip:" + OPTION + " SIP/2.0\n")
-            fich.close()
+            Log().Log('send', TO, SEND)
 
-        elif METHOD == 'BYE':
-            fich = open(UA['log_path'], 'a')
-            Time = time.strftime('%Y%m%d%H%M%S', time.gmtime())
-            fich.write(Time + ' Finishing...\r\n')
-            fich.close()
 # Cerramos todo
 my_socket.close()
