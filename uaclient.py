2dc32e395d057327943c8f369f9a5b9548df89a3
Alba <alba@ubuntu.(none)>
2015-01-09 15:49:15 +0100

    uaclient.py final parte básica, con pep8

--- uaclient.py
+++ uaclient.py
@@ -4,24 +4,12 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 from uaserver import XMLHandler
+from uaserver import Log
 import sys
 import socket
 import os
 import time
 
-
-class Log(ContentHandler):
-    def Log(self, tipo, to, message):
-        fich = open(UA['log_path'], 'a')
-        Time = time.strftime('%Y%m%d%H%M%S', time.gmtime())
-        message = message.replace('\r\n', ' ') + '\n'
-        if tipo == 'send':
-            fich.write(Time + ' Sent to ' + to + message)
-        elif tipo == 'receive':
-            fich.write(Time + ' Received from ' + to + message)
-        elif tipo == 'error':
-            fich.write(Time + "Error: No server listening at " + message)
-        fich.close()
 
 if __name__ == "__main__":
     """
@@ -42,7 +30,7 @@ if __name__ == "__main__":
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((UA['regproxy_ip'], int(UA['regproxy_puerto'])))
-    TO = UA['regproxy_ip'] + ':' + UA['regproxy_puerto'] + ': '
+    TO = UA['regproxy_ip'] + ':' + UA['regproxy_puerto']
     # Enviamos diferentes cosas según el método
     if METHOD == "REGISTER":
         try:
@@ -54,12 +42,13 @@ if __name__ == "__main__":
         SEND = METHOD + " sip:" + LINE + " SIP/2.0\r\n" + EXPIRES
         print "Enviado:\r\n" + METHOD + " sip:" + LINE + " SIP/2.0\n" + EXPIRES
         my_socket.send(SEND)
-        Log().Log('send', TO, SEND)
+        Log().Log(UA['log_path'], 'Init/end', TO, 'Starting...')
+        Log().Log(UA['log_path'], 'send', TO, SEND)
 
     elif METHOD == "INVITE":
         print "Enviando:\r\n" + METHOD + " sip:" + OPTION + " SIP/2.0"
-        print "Content type: application/sdp\r\n\r\n"
-        APPLICATION = "Content type:application/sdp" + "\r\n\r\n"
+        print "Content-type: application/sdp\r\n\r\n"
+        APPLICATION = "Content-type: application/sdp\r\n\r\n"
         HEAD = METHOD + " sip:" + OPTION + " SIP/2.0\r\n" + APPLICATION
         O = "o=" + UA['account_username'] + " " + UA['uaserver_ip'] + " \r\n"
         M = "m=audio " + UA['rtpaudio_puerto'] + " RTP\r\n"
@@ -67,26 +56,32 @@ if __name__ == "__main__":
         print BODY
         SEND = HEAD + BODY
         my_socket.send(SEND)
-        Log().Log('send', TO, SEND)
+        Log().Log(UA['log_path'], 'send', TO, SEND)
 
     elif METHOD == "BYE":
         SEND = METHOD + " sip:" + OPTION + " SIP/2.0\r\n\r\n"
         print "Enviando:\r\n" + METHOD + " sip:" + OPTION + " SIP/2.0"
         my_socket.send(SEND)
-        Log().Log('send', TO, SEND)
+        Log().Log(UA['log_path'], 'send', TO, SEND)
+    else:
+        sys.exit('Usage: Method Not Allowed')
 
     try:
         data = my_socket.recv(1024)
     except socket.error:
         SOCKET_ERROR = UA['regproxy_ip'] + " PORT:" + UA['regproxy_puerto']
         sys.exit("Error: No server listening at " + SOCKET_ERROR)
-        Log().Log('error', TO, SOCKET_ERROR)
+        Log().Log(UA['log_path'], 'error', ' ', SOCKET_ERROR)
 
     print 'Recibido\r\n', data
-    Log().Log('receive', TO, data)
+    Log().Log(UA['log_path'], 'receive', TO, data)
 
-    if data == "SIP/2.0 404 User Not Found\r\n\r\n":
-        sys.exit(" ")
+    r400 = "SIP/2.0 400 Bad Request\r\n\r\n"
+    r404 = "SIP/2.0 404 User Not Found\r\n\r\n"
+    r_404 = "SIP/2.0 404 Not Found\r\n\r\n"
+    r405 = "SIP/2.0 405 Method Not Allowed\r\n\r\n"
+    if data == r400 or data == r404 or data == r405 or data == r_404:
+        sys.exit()
     else:
         if METHOD == "INVITE":
             #He recibido los tres mensajes a la vez, en una misma línea
@@ -94,15 +89,18 @@ if __name__ == "__main__":
             SEND = METHOD + " sip:" + OPTION + " SIP/2.0\r\n\r\n"
             print "Enviando: " + METHOD + " sip:" + OPTION + " SIP/2.0\r\n\r\n"
             my_socket.send(SEND)
-            receptor_Ip = data.split("o=")[1].split(" ")[1].split("s")[0]
-            receptor_Puerto = data.split("m=")[1].split(" ")[1]
+            rcv_Ip = data.split("o=")[1].split(" ")[1].split("s")[0]
+            rcv_Port = data.split("m=")[1].split(" ")[1]
             os.system("chmod 777 mp32rtp")
-            aEjecutar = './mp32rtp -i ' + receptor_Ip + ' -p '
-            aEjecutar += receptor_Puerto + " < " + UA['audio_path']
+            aEjecutar = './mp32rtp -i ' + rcv_Ip + ' -p '
+            aEjecutar += rcv_Port + " < " + UA['audio_path']
             print "Vamos a ejecutar", aEjecutar
             os.system(aEjecutar)
             print("Ha terminado la ejecución de fich de audio")
-            Log().Log('send', TO, SEND)
+            Log().Log(UA['log_path'], 'send', TO, SEND)
+
+        elif METHOD == 'BYE':
+            Log().Log(UA['log_path'], 'Init/end', ' ', 'Finishing...\n')
 
 # Cerramos todo
 my_socket.close()
