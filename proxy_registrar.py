f404e96594abb0e5aafbe08b0f950b135c6e116d
Alba <alba@ubuntu.(none)>
2015-01-09 15:50:30 +0100

    proxy_registrar.py final parte básica, con pep8

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -43,6 +43,7 @@ class XMLHandler(ContentHandler):
     def get_tags(self):
         return self.config
 
+
 class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
     SIP server class
@@ -50,11 +51,11 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     def Search_User(self, user):
         for Client in Registro:
             if user == Client:
-                To['ip'] = Registro[Client][0]                     
-                To['puerto'] = Registro[Client][1]
-        
+                Uas['ip'] = Registro[Client][0]
+                Uas['puerto'] = Registro[Client][1]
+
     def handle(self):
-        DESTINO = self.client_address[0] + ' ' + str(self.client_address[1])
+        UAC = self.client_address[0] + ' ' + str(self.client_address[1])
         while 1:
             line = self.rfile.read()
             if not line:
@@ -63,7 +64,9 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
             line_partida = line.split(":")
             Method = line_partida[0].split(" ")[0]
             if not Method in METHODS:
-                self.wfile.write("SIP/2.0 405 Method Not Allowed\r\n\r\n")
+                response = "SIP/2.0 405 Method Not Allowed\r\n\r\n"
+                self.wfile.write(response)
+                Log().Log(PR['log_logpath'], 'send', UAC, response)
             #Si tenemos Register, procedemos a rellenar nuestro Registro
             elif Method == "REGISTER":
                 #Primero comprobamos los tiempos de expiración
@@ -72,110 +75,123 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
                     Tiempo = Registro[Client][2]
                     if time.time() >= Tiempo:
                         print Client + " Borrado\r\n"
-                        del Registro[Client]                        
+                        del Registro[Client]
                 Clave = line_partida[1]
                 Puerto_UA = line_partida[2].split(" ")[0]
                 Expires = line_partida[3].split(" ")[1]
                 Time = time.time() + int(Expires)
                 Valores = [self.client_address[0], Puerto_UA, Time]
                 Registro[Clave] = Valores
-                print Clave + " Registrado\r\n"
                 #Si entramos con un valor a 0, somos borrados
                 if int(Expires) == 0:
                     del Registro[Clave]
                     print Client + " Borrado\r\n"
+                else:
+                    print Clave + " Registrado\r\n"
                 self.register2file()
-                respuesta = "SIP/2.0 200 OK\r\n\r\n"
-                self.wfile.write(respuesta)
-                Log().Log_write(PR['log_logpath'], 'receive', DESTINO, line)
-                Log().Log_write(PR['log_logpath'], 'send', DESTINO, respuesta)
-                                
+                response = "SIP/2.0 200 OK\r\n\r\n"
+                self.wfile.write(response)
+                Log().Log(PR['log_logpath'], 'receive', UAC, line)
+                Log().Log(PR['log_logpath'], 'send', UAC, response)
+
             elif Method == "INVITE":
-                Log().Log_write(PR['log_logpath'], 'receive', DESTINO, line)
+                Log().Log(PR['log_logpath'], 'receive', UAC, line)
                 To_name = line_partida[1].split(" ")[0]
                 self.Search_User(To_name)
-                if Registro.has_key(To_name) == False:
-                    self.wfile.write("SIP/2.0 404 User Not Found\r\n\r\n")
+                if not To_name in Registro:
+                    response = "SIP/2.0 404 User Not Found\r\n\r\n"
+                    self.wfile.write(response)
+                    Log().Log(PR['log_logpath'], 'send', UAC, response)
                 else:
-                    #Creamos socket, configuramos y lo atamos a un 
-                    #servidor/puerto, my_skt es my_socket, por pep8 my_skt
-                    my_skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                    my_skt.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                    my_skt.connect((To['ip'], int(To['puerto'])))
-                    my_skt.send(line)
-                    TO = To['ip'] + ' ' + To['puerto']
-                    Log().Log_write(PR['log_logpath'], 'send', TO, line)
+                    #Creamos socket, configuramos y lo atamos a un
+                    #servidor/puerto, my_sk es my_socket, por pep8 my_sk
+                    my_sk = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                    my_sk.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                    my_sk.connect((Uas['ip'], int(Uas['puerto'])))
+                    my_sk.send(line)
+                    UAS = Uas['ip'] + ' ' + Uas['puerto']
+                    Log().Log(PR['log_logpath'], 'send', UAS, line)
                     print "INVITE Enviado a " + To_name + '\r\n'
+                    error = False
                     try:
-                        data = my_skt.recv(1024)
+                        data = my_sk.recv(1024)
                     except socket.error:
-                        SKT_ERROR = To_IP + " PORT:" + To_Port
-                        sys.exit("No server listening at " + SKT_ERROR)
-                        Log().Log_write(UA['log_path'], 'error', TO, SKT_ERROR)
-                    print 'Respuesta recibida de ' + To_name + '\r\n'
-                    #Tenemos directamente una conexión con el que nos abrió 
-                    # el socket enviando el INVITE
-                    self.wfile.write(data)
-                    Log().Log_write(PR['log_logpath'], 'receive', TO, data)
-                    Log().Log_write(PR['log_logpath'], 'send', DESTINO, data)
-                               
+                        response = "SIP/2.0 404 Not Found\r\n\r\n"
+                        self.wfile.write(response)
+                        Log().Log(PR['log_logpath'], 'send', UAC, response)
+                        error = True
+                    if not error:
+                        print 'Respuesta recibida de ' + To_name + '\r\n'
+                        #Tenemos directamente una conexión con el que nos abrió
+                        # el socket enviando el INVITE
+                        self.wfile.write(data)
+                        Log().Log(PR['log_logpath'], 'receive', UAS, data)
+                        Log().Log(PR['log_logpath'], 'send', UAC, data)
+
             elif Method == "ACK":
-                Log().Log_write(PR['log_logpath'], 'receive', DESTINO, line)
+                Log().Log(PR['log_logpath'], 'receive', UAC, line)
                 To_name = line_partida[1].split(" ")[0]
                 self.Search_User(To_name)
                 print 'ACK recibido y reenviado a ' + To_name + '\r\n'
-                TO = To['ip'] + ' ' + To['puerto']
-                Log().Log_write(PR['log_logpath'], 'send', TO, line)
+                UAS = Uas['ip'] + ' ' + Uas['puerto']
+                Log().Log(PR['log_logpath'], 'send', UAS, line)
                 #Creamos socket, configuramos y lo atamos a un servidor/puerto
-                my_skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                my_skt.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                my_skt.connect((To['ip'], int(To['puerto'])))
-                my_skt.send(line)
-                
+                my_sk = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                my_sk.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                my_sk.connect((Uas['ip'], int(Uas['puerto'])))
+                my_sk.send(line)
+
             elif Method == "BYE":
-                Log().Log_write(PR['log_logpath'], 'receive', DESTINO, line)
+                Log().Log(PR['log_logpath'], 'receive', UAC, line)
                 To_name = line_partida[1].split(" ")[0]
                 self.Search_User(To_name)
-                if Registro.has_key(To_name) == False:
-                    self.wfile.write("SIP/2.0 400 Bad Request\r\n\r\n")
+                if not To_name in Registro:
+                    response = "SIP/2.0 400 Bad Request\r\n\r\n"
+                    self.wfile.write(response)
+                    Log().Log(PR['log_logpath'], 'send', UAC, response)
                 else:
+                    UAS = Uas['ip'] + ' ' + Uas['puerto']
                     print 'BYE enviado a ' + To_name + '\r\n'
-                    TO = To['ip'] + ' ' + To['puerto']
-                    Log().Log_write(PR['log_logpath'], 'send', TO, line)
+                    Log().Log(PR['log_logpath'], 'send', UAS, line)
                     #Creamos socket, configuramos y lo atamos a un servidor
-                    my_skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                    my_skt.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                    my_skt.connect((To['ip'], int(To['puerto'])))
-                    my_skt.send(line)
+                    my_sk = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                    my_sk.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                    my_sk.connect((Uas['ip'], int(Uas['puerto'])))
+                    my_sk.send(line)
+                    error = False
                     try:
-                        data = my_skt.recv(1024)
+                        data = my_sk.recv(1024)
                     except socket.error:
-                        SKT_ERROR = To_IP + " PORT:" + To_Port
-                        sys.exit("No server listening at " + SKT_ERROR)
-                        Log().Log_write(UA['log_path'], 'error', TO, SKT_ERROR)
-                    print 'Respuesta recibida de ' + To_name + '\r\n'
-                    #Tenemos directamente una conexión con el que nos abrió 
-                    # el socket enviando el INVITE
-                    self.wfile.write(data)
-                    # cerramos el socket al final del todo, no lo utilizremos +
-                    my_skt.close()
-                    Log().Log_write(PR['log_logpath'], 'receive', TO, data)
-                    Log().Log_write(PR['log_logpath'], 'send', DESTINO, data)
-                               
+                        response = "SIP/2.0 404 Not Found\r\n\r\n"
+                        self.wfile.write(response)
+                        Log().Log(PR['log_logpath'], 'send', UAC, response)
+                        error = True
+                    if not error:
+                        print 'Respuesta recibida de ' + To_name + '\r\n'
+                        #Tenemos directamente una conexión con el que nos abrió
+                        # el socket enviando el INVITE
+                        self.wfile.write(data)
+                        # cerramos el socket, no lo utilizremos +
+                        my_sk.close()
+                        Log().Log(PR['log_logpath'], 'receive', UAS, data)
+                        Log().Log(PR['log_logpath'], 'send', UAC, data)
+
             else:
-                self.wfile.write("SIP/2.0 400 Bad Request\r\n\r\n")
-                 
+                response = "SIP/2.0 400 Bad Request\r\n\r\n"
+                self.wfile.write(response)
+                Log().Log(PR['log_logpath'], 'send', UAS, response)
+
     def register2file(self):
         """ Método que escribirá en nuestro fichero"""
-        fichero = open('database.txt', 'w')
-        fichero.write("User\tIP\tPuerto\tExpires\n")
+        fich = open('database.txt', 'w')
+        fich.write("User\tIP\tPuerto\tExpires\n")
         for Client in Registro:
             IP = Registro[Client][0]
             Port = Registro[Client][1]
             Tiempo = time.gmtime(Registro[Client][2])
             Time = time.strftime('%Y%m%d%H%M%S', Tiempo)
-            fichero.write(Client + '\t' + IP + '\t' + Port + '\t' + Time + '\n')
-        fichero.close()
+            fich.write(Client + '\t' + IP + '\t' + Port + '\t' + Time + '\n')
+        fich.close()
 
 if __name__ == "__main__":
     try:
@@ -189,13 +205,14 @@ if __name__ == "__main__":
     PR = cHandler.get_tags()
     LISTENING_PORT = PR['server_puerto']
     Registro = {}
-    To = {}
+    Uas = {}
     # Creamos servidor Registrar y escuchamos
-    serv = SocketServer.UDPServer(("", int(LISTENING_PORT)), SIPRegisterHandler)
-    # Abrimos fichero log_proxy.txt
-    fich = open(PR['log_logpath'], 'a')
-    Time = time.strftime('%Y%m%d%H%M%S', time.gmtime())
-    fich.write(Time + ' Starting...\n')
-    fich.close()
+    s = SocketServer.UDPServer(("", int(LISTENING_PORT)), SIPRegisterHandler)
+    # Escribimos inicio log_proxy.txt
+    Log().Log(PR['log_logpath'], 'Init/end', ' ', 'Starting...')
     print "Server " + PR['server_name'] + "listening at port " + LISTENING_PORT
-    serv.serve_forever()
+    try:
+        s.serve_forever()
+    except KeyboardInterrupt:
+        Log().Log(PR['log_logpath'], 'Init/end', Uas, 'Finishing...\n')
+        print ('Servidor Interrumpido (Ctrl + C)')
